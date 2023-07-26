4decdb12e9adbf54a0d66d5c05e94e65e84266af
Andrewalufkin <andrewalufkin@gmail.com>
2023-07-25 19:05:40 -0500

    Added appspec.yml and scripts for deployment

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /var/www/html   # Change to the frontend destination directory
+npm install        # Install dependencies
---
+++ scripts/scripts.start_server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /var/www/html   # Change to the frontend destination directory
+npm start          # Or any other command you use to start your Node.js server
