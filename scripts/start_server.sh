6483ea6ba9a764647deab5cc9896305b58d3e1a6
Andrewalufkin <andrewalufkin@gmail.com>
2023-08-12 03:38:40 -0500

    Fixed start server

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-cd /home/ec2-user/backend   # Change to the backend destination directory
-node .          # Start the Node.js server
+# Full path to the Node.js executable
+NODE_PATH=$(which node)
+
+# Change to the backend destination directory
+cd /home/ec2-user/backend
+
+# Start the Node.js server using the full path to the executable
+$NODE_PATH .
