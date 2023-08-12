26a2bfff406140fac7bdc213b8ce42a30c053ffb
Andrewalufkin <andrewalufkin@gmail.com>
2023-08-12 01:44:30 -0500

    Fixed npm again.

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /home/ec2-user/frontend/build   # Change to the frontend destination directory
-/.nvm/versions/node/v20.5.1/bin/npm install        # Install dependencies
+npm install        # Install dependencies
 
 cd ../backend # Change to the backened destination directory
-/.nvm/versions/node/v20.5.1/bin/npm install # Install dependencies
+npm install # Install dependencies
--- scripts/scripts.start_server.sh
+++ scripts/scripts.start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /home/ec2-user/backend/build   # Change to the backend destination directory
-/.nvm/versions/node/v20.5.1/bin/npm start          # Start the Node.js server
+npm start          # Start the Node.js server
