5644fa51436b5f7b2caa793cae20ffd67be99990
Andrewalufkin <andrewalufkin@gmail.com>
2024-01-12 21:05:33 -0600

    Trying to debug validate_service.sh

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-APP_URL="https://ec2-54-211-142-255.compute-1.amazonaws.com"
+APP_URL="http://ec2-54-211-142-255.compute-1.amazonaws.com"
 
 # Perform a simple health check to verify that the application is running and responding
 HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" "$APP_URL")
+
+# Debugging with HTTP_STATUS
+echo "HTTP Status: $HTTP_STATUS"
+RESPONSE=$(curl -s "$APP_URL")
+echo "Response: $RESPONSE"
 
 if [ "$HTTP_STATUS" -eq 200 ]; then
   echo "Application health check succeeded. Deployment was successful."
